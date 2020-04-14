1d58e2cf4c81345b65877a992ba12dba327ac6c3
Saurav <sauravsrivastav103@gmail.com>
2020-04-14 23:02:19 +0530

    Removed the documented code from starterbot.py

--- starterbot.py
+++ starterbot.py
@@ -17,11 +17,6 @@ greetings = ['hi', 'hello', 'hello there', 'hey']
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
 def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
             user_id, message = parse_direct_mention(event["text"])
@@ -30,18 +25,11 @@ def parse_bot_commands(slack_events):
     return None, None
 
 def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
     matches = re.search(MENTION_REGEX, message_text)
     # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
     # Default response is help text for the user
     default_response = "Not sure what you mean. Try *{}* to know about my capabilities.".format(EXAMPLE_COMMAND)
 
